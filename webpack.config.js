7971687fdea4c0bbadccc5c8cfb1d844b767b9f4
Ryan Florence <rpflorence@gmail.com>
2015-11-17 00:39:31 -0800

    get external libs out of the umd build

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,23 @@ module.exports = {
     libraryTarget: 'umd'
   },
 
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      },
+      'react-router': {
+        root: 'ReactRouter',
+        commonjs2: 'react-router',
+        commonjs: 'react-router',
+        amd: 'react-router'
+      }
+    }
+  ],
+
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel?stage=0&loose=all' }
